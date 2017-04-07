25344d34610d32cfbcdff3052688ad3b629992d9
TheLucksmith <austin.luchsinger01@utrgv.edu>
2017-04-07 14:48:53 -0500

    added package comment line

--- Nonads/src/nonads/Nonads.java
+++ Nonads/src/nonads/Nonads.java
@@ -1,7 +1,7 @@
 /* To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nonads;
+package src.nonads;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
